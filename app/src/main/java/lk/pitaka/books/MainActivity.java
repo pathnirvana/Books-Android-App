7f91ed8254ba96efcd3054c5f668d16464caa02b
pathnirvana <pathnirvana@gmail.com>
2023-05-11 11:30:36 +0700

    androidx and gradle 8

--- app/src/main/java/lk/pitaka/books/MainActivity.java
+++ app/src/main/java/lk/pitaka/books/MainActivity.java
@@ -1,8 +1,8 @@
 package lk.pitaka.books;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
